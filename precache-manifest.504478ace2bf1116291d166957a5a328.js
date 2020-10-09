96d841d06933b717e40054a713eca8694afc09da
yuval-david <yuvid0224@gmail.com>
2020-10-09 23:43:36 +0300

    Updates

---
+++ precache-manifest.504478ace2bf1116291d166957a5a328.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9b1df75383032e0c9f67f996001500",
+    "url": "/Yuval-websites/index.html"
+  },
+  {
+    "revision": "9738094acd43f8d5b05d",
+    "url": "/Yuval-websites/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "636658cd1d79f52e7e6f",
+    "url": "/Yuval-websites/static/css/main.94353d11.chunk.css"
+  },
+  {
+    "revision": "9738094acd43f8d5b05d",
+    "url": "/Yuval-websites/static/js/2.af1275bc.chunk.js"
+  },
+  {
+    "revision": "42b78d8d1949b97817c905bbe4fd6fc4",
+    "url": "/Yuval-websites/static/js/2.af1275bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "636658cd1d79f52e7e6f",
+    "url": "/Yuval-websites/static/js/main.089f1840.chunk.js"
+  },
+  {
+    "revision": "f7f3139590611fada15e",
+    "url": "/Yuval-websites/static/js/runtime-main.8887e36c.js"
+  },
+  {
+    "revision": "504b535c80c8095184901c10e561233b",
+    "url": "/Yuval-websites/static/media/Curve-Loading.504b535c.gif"
+  },
+  {
+    "revision": "7351de6a85c3ebb892b9d92dca3cde45",
+    "url": "/Yuval-websites/static/media/GroupTasks.7351de6a.mp4"
+  },
+  {
+    "revision": "238528fe399f02025dcb62791523d859",
+    "url": "/Yuval-websites/static/media/KCbeauty.238528fe.mp4"
+  },
+  {
+    "revision": "8bd93718b4381cc7f8b52199a714ab10",
+    "url": "/Yuval-websites/static/media/Yuval1.8bd93718.jpg"
+  },
+  {
+    "revision": "d9f51b3477d98d072419fd968e79978c",
+    "url": "/Yuval-websites/static/media/coin.d9f51b34.jpg"
+  },
+  {
+    "revision": "169d12a50851b7167c7cf6888f20ec0c",
+    "url": "/Yuval-websites/static/media/coins.169d12a5.mp4"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Yuval-websites/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a8a2f61b35818b416077167a54f881df",
+    "url": "/Yuval-websites/static/media/rupaul.a8a2f61b.mp4"
+  },
+  {
+    "revision": "c364a228268213d105dd3f5401690559",
+    "url": "/Yuval-websites/static/media/super.c364a228.mp4"
+  },
+  {
+    "revision": "089c4ffc720dc6a452c1bcd8abe03a03",
+    "url": "/Yuval-websites/static/media/vacations.089c4ffc.mp4"
+  }
+]);
